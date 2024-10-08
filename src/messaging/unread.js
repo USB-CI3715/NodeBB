7239ac4cbbe290dfe4cc816b7a76c6b1715c8fcd
A6575 <astridaalvaradoa@gmail.com>
2024-10-08 17:53:15 -0400

    Eliminar uso de interfaces para tipado en TS. Luego del cambio aun posee errores de ESLint

--- src/messaging/unread.js
+++ src/messaging/unread.js
@@ -11,8 +11,8 @@ const __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, 
 };
 const __generator = (this && this.__generator) || function (thisArg, body) {
 	let _ = { label: 0, sent: function () { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }; let f; let y; let t; let
-		g;
-	return g = { next: verb(0), throw: verb(1), return: verb(2) }, typeof Symbol === 'function' && (g[Symbol.iterator] = function () { return this; }), g;
+		g = Object.create((typeof Iterator === 'function' ? Iterator : Object).prototype);
+	return g.next = verb(0), g.throw = verb(1), g.return = verb(2), typeof Symbol === 'function' && (g[Symbol.iterator] = function () { return this; }), g;
 	function verb(n) { return function (v) { return step([n, v]); }; }
 	function step(op) {
 		if (f) throw new TypeError('Generator is already executing.');
@@ -39,125 +39,170 @@ const __generator = (this && this.__generator) || function (thisArg, body) {
 		if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
 	}
 };
+const __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+	if (pack || arguments.length === 2) {
+		for (var i = 0, l = from.length, ar; i < l; i++) {
+			if (ar || !(i in from)) {
+				if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+				ar[i] = from[i];
+			}
+		}
+	}
+	return to.concat(ar || Array.prototype.slice.call(from));
+};
 Object.defineProperty(exports, '__esModule', { value: true });
+exports.Messaging = Messaging;
 const database_1 = require('../database');
 const socket_io_1 = require('../socket.io');
 
-module.exports = function (Messaging) {
+function Messaging() {
 	const _this = this;
-	Messaging.getUnreadCount = function (uid) {
-		return __awaiter(_this, void 0, void 0, function () {
-			return __generator(this, (_a) => {
-				switch (_a.label) {
-					case 0:
-						if (!(parseInt(uid, 10) > 0)) {
-							return [2 /* return */, 0];
-						}
-						return [4 /* yield */, database_1.default.sortedSetCard('uid:'.concat(uid, ':chat:rooms:unread'))];
-					case 1: return [2 /* return */, _a.sent()];
-				}
-			});
-		});
-	};
-	Messaging.pushUnreadCount = function (uids, data) {
-		if (data === void 0) { data = null; }
-		return __awaiter(_this, void 0, void 0, function () {
-			return __generator(this, (_a) => {
-				if (!Array.isArray(uids)) {
-					uids = [uids];
-				}
-				uids = uids.filter(uid => parseInt(uid, 10) > 0);
-				if (!uids.length) {
-					return [2];
-				}
-				uids.forEach((uid) => {
-					socket_io_1.default.in('uid_'.concat(uid)).emit('event:unread.updateChatCount', data);
+	return {
+		getUnreadCount: function (uid) {
+			return __awaiter(_this, void 0, void 0, function () {
+				let result;
+				return __generator(this, (_a) => {
+					switch (_a.label) {
+						case 0:
+							if (!(parseInt(uid, 10) > 0)) {
+								return [2 /* return */, 0];
+							}
+							return [4 /* yield */, database_1.default.sortedSetCard('uid:'.concat(uid, ':chat:rooms:unread'))];
+						case 1:
+							result = _a.sent();
+							return [2 /* return */, result];
+					}
 				});
-				return [2];
 			});
-		});
-	};
-	Messaging.markRead = function (uid, roomId) {
-		return __awaiter(_this, void 0, void 0, function () {
-			return __generator(this, (_a) => {
-				switch (_a.label) {
-					case 0: return [4 /* yield */, Promise.all([
-						database_1.default.sortedSetRemove('uid:'.concat(uid, ':chat:rooms:unread'), roomId),
-						database_1.default.setObjectField('uid:'.concat(uid, ':chat:rooms:read'), roomId, Date.now()),
-					])];
-					case 1:
-						_a.sent();
+		},
+		pushUnreadCount: function (uids_1) {
+			const args_1 = [];
+			for (let _i = 1; _i < arguments.length; _i++) {
+				args_1[_i - 1] = arguments[_i];
+			}
+			return __awaiter(_this, __spreadArray([uids_1], args_1, true), void 0, function (uids, data) {
+				if (data === void 0) { data = null; }
+				return __generator(this, (_a) => {
+					if (!Array.isArray(uids)) {
+						uids = [uids];
+					}
+					uids = uids.filter(uid => parseInt(uid, 10) > 0);
+					if (!uids.length) {
 						return [2];
-				}
+					}
+					uids.forEach((uid) => {
+						socket_io_1.default.in('uid_'.concat(uid)).emit('event:unread.updateChatCount', data);
+					});
+					return [2];
+				});
 			});
-		});
-	};
-	Messaging.hasRead = function (uids, roomId) {
-		return __awaiter(_this, void 0, void 0, function () {
-			let roomData; let _a; let userTimestamps_1; let mids; let lastMsgTimestamp_1; let
-				isMembers;
-			return __generator(this, (_b) => {
-				switch (_b.label) {
-					case 0:
-						if (!uids.length) {
-							return [2 /* return */, []];
-						}
-						return [4 /* yield */, Messaging.getRoomData(roomId)];
-					case 1:
-						roomData = _b.sent();
-						if (!roomData) {
-							return [2 /* return */, uids.map(() => false)];
-						}
-						if (!roomData.public) return [3 /* break */, 3];
-						return [4 /* yield */, Promise.all([
-							database_1.default.getObjectsFields(uids.map(uid => 'uid:'.concat(uid, ':chat:rooms:read')), [roomId]),
-							database_1.default.getSortedSetRevRangeWithScores('chat:room:'.concat(roomId, ':mids'), 0, 0),
+		},
+		markRead: function (uid, roomId) {
+			return __awaiter(_this, void 0, void 0, function () {
+				return __generator(this, (_a) => {
+					switch (_a.label) {
+						case 0: return [4 /* yield */, Promise.all([
+							database_1.default.sortedSetRemove('uid:'.concat(uid, ':chat:rooms:unread'), roomId),
+							database_1.default.setObjectField('uid:'.concat(uid, ':chat:rooms:read'), roomId, Date.now()),
 						])];
-					case 2:
-						_a = _b.sent(), userTimestamps_1 = _a[0], mids = _a[1];
-						lastMsgTimestamp_1 = mids[0] ? mids[0].score : 0;
-						return [2 /* return */, uids.map((uid, index) => !userTimestamps_1[index] ||
-                                !userTimestamps_1[index][roomId] ||
-                                parseInt(userTimestamps_1[index][roomId], 10) > lastMsgTimestamp_1)];
-					case 3: return [4 /* yield */, database_1.default.isMemberOfSortedSets(uids.map(uid => 'uid:'.concat(uid, ':chat:rooms:unread')), roomId)];
-					case 4:
-						isMembers = _b.sent();
-						return [2 /* return */, uids.map((uid, index) => !isMembers[index])];
-				}
-			});
-		});
-	};
-	Messaging.markAllRead = function (uid) {
-		return __awaiter(_this, void 0, void 0, function () {
-			return __generator(this, (_a) => {
-				switch (_a.label) {
-					case 0: return [4 /* yield */, database_1.default.delete('uid:'.concat(uid, ':chat:rooms:unread'))];
-					case 1:
-						_a.sent();
-						return [2];
-				}
-			});
-		});
-	};
-	Messaging.markUnread = function (uids, roomId) {
-		return __awaiter(_this, void 0, void 0, function () {
-			let exists; let
-				keys;
-			return __generator(this, (_a) => {
-				switch (_a.label) {
-					case 0: return [4 /* yield */, Messaging.roomExists(roomId)];
-					case 1:
-						exists = _a.sent();
-						if (!exists) {
+						case 1:
+							_a.sent();
 							return [2];
-						}
-						keys = uids.map(uid => 'uid:'.concat(uid, ':chat:rooms:unread'));
-						return [4 /* yield */, database_1.default.sortedSetsAdd(keys, Date.now(), roomId)];
-					case 2:
-						_a.sent();
-						return [2];
-				}
+					}
+				});
 			});
-		});
+		},
+		hasRead: function (uids, roomId) {
+			return __awaiter(_this, void 0, void 0, function () {
+				let roomData; let _a; let userTimestamps_1; let mids; let lastMsgTimestamp_1; let
+					isMembers;
+				return __generator(this, function (_b) {
+					switch (_b.label) {
+						case 0:
+							if (!uids.length) {
+								return [2 /* return */, []];
+							}
+							return [4 /* yield */, this.getRoomData(roomId)];
+						case 1:
+							roomData = _b.sent();
+							if (!roomData) {
+								return [2 /* return */, uids.map(() => false)];
+							}
+							if (!roomData.public) return [3 /* break */, 3];
+							return [4 /* yield */, Promise.all([
+								database_1.default.getObjectsFields(uids.map(uid => 'uid:'.concat(uid, ':chat:rooms:read')), [roomId]),
+								database_1.default.getSortedSetRevRangeWithScores('chat:room:'.concat(roomId, ':mids'), 0, 0),
+							])];
+						case 2:
+							_a = _b.sent(), userTimestamps_1 = _a[0], mids = _a[1];
+							lastMsgTimestamp_1 = mids[0] ? mids[0].score : 0;
+							return [2 /* return */, uids.map((uid, index) => !userTimestamps_1[index] ||
+                                    !userTimestamps_1[index][roomId] ||
+                                    parseInt(userTimestamps_1[index][roomId], 10) > lastMsgTimestamp_1)];
+						case 3: return [4 /* yield */, database_1.default.isMemberOfSortedSets(uids.map(uid => 'uid:'.concat(uid, ':chat:rooms:unread')), roomId)];
+						case 4:
+							isMembers = _b.sent();
+							return [2 /* return */, uids.map((uid, index) => !isMembers[index])];
+					}
+				});
+			});
+		},
+		markAllRead: function (uid) {
+			return __awaiter(_this, void 0, void 0, function () {
+				return __generator(this, (_a) => {
+					switch (_a.label) {
+						case 0: return [4 /* yield */, database_1.default.delete('uid:'.concat(uid, ':chat:rooms:unread'))];
+						case 1:
+							_a.sent();
+							return [2];
+					}
+				});
+			});
+		},
+		markUnread: function (uids, roomId) {
+			return __awaiter(_this, void 0, void 0, function () {
+				let exists; let
+					keys;
+				return __generator(this, function (_a) {
+					switch (_a.label) {
+						case 0: return [4 /* yield */, this.roomExists(roomId)];
+						case 1:
+							exists = _a.sent();
+							if (!exists) {
+								return [2];
+							}
+							keys = uids.map(uid => 'uid:'.concat(uid, ':chat:rooms:unread'));
+							return [4 /* yield */, database_1.default.sortedSetsAdd(keys, Date.now(), roomId)];
+						case 2:
+							_a.sent();
+							return [2];
+					}
+				});
+			});
+		},
+		roomExists: function (roomId) {
+			return __awaiter(_this, void 0, void 0, function () {
+				let exists;
+				return __generator(this, (_a) => {
+					switch (_a.label) {
+						case 0: return [4 /* yield */, database_1.default.exists('chat:room:'.concat(roomId))];
+						case 1:
+							exists = _a.sent();
+							return [2 /* return */, exists > 0];
+					}
+				});
+			});
+		},
+		getRoomData: function (roomId) {
+			return __awaiter(_this, void 0, void 0, function () {
+				return __generator(this, (_a) => {
+					switch (_a.label) {
+						case 0: return [4 /* yield */, database_1.default.getObject('chat:room:'.concat(roomId))];
+						case 1: return [2 /* return */, _a.sent()];
+					}
+				});
+			});
+		},
 	};
-};
+}
+
--- src/messaging/unread.ts
+++ src/messaging/unread.ts
@@ -2,93 +2,95 @@
 
 import db from '../database';
 import io from '../socket.io';
-interface Messaging {
-    getUnreadCount(uid: string | number): Promise<number>;
-    pushUnreadCount(uids: string[] | string, data?: any): Promise<void>;
-    markRead(uid: string | number, roomId: string): Promise<void>;
-    hasRead(uids: string[], roomId: string): Promise<boolean[]>;
-    markAllRead(uid: string | number): Promise<void>;
-    markUnread(uids: string[], roomId: string): Promise<void>;
-    roomExists(roomId: string): Promise<boolean>;
-    getRoomData(roomId: string): Promise<any>;
-}
 
-module.exports = function (Messaging: Messaging) {
-    Messaging.getUnreadCount = async (uid: string | number): Promise<number> => {
-        if (!(parseInt(uid as string, 10) > 0)) {
-            return 0;
-        }
-        
-        return await db.sortedSetCard(`uid:${uid}:chat:rooms:unread`);
-    };
-    
-    Messaging.pushUnreadCount = async (uids: string[] | string, data: any = null): Promise<void> => {
-        if (!Array.isArray(uids)) {
-            uids = [uids];
-        }
-        
-        uids = uids.filter(uid => parseInt(uid as string, 10) > 0);
-        
-        if (!uids.length) {
-            return;
-        }
-        
-        uids.forEach((uid) => {
-            io.in(`uid_${uid}`).emit('event:unread.updateChatCount', data);
-        });
-    };
-    
-    Messaging.markRead = async (uid: string | number, roomId: string): Promise<void> => {
-        await Promise.all([
-            db.sortedSetRemove(`uid:${uid}:chat:rooms:unread`, roomId),
-            db.setObjectField(`uid:${uid}:chat:rooms:read`, roomId, Date.now()),
-        ]);
-    };
-    
-    Messaging.hasRead = async (uids: string[], roomId: string): Promise<boolean[]> => {
-        if (!uids.length) {
-            return [];
-        }
-        
-        const roomData = await Messaging.getRoomData(roomId);
-        if (!roomData) {
-            return uids.map(() => false);
-        }
-        
-        if (roomData.public) {
-            const [userTimestamps, mids] = await Promise.all([
-                db.getObjectsFields(uids.map(uid => `uid:${uid}:chat:rooms:read`), [roomId]),
-                db.getSortedSetRevRangeWithScores(`chat:room:${roomId}:mids`, 0, 0),
-            ]);
-            
-            const lastMsgTimestamp = mids[0] ? mids[0].score : 0;
-            return uids.map(
-                (uid, index) =>
-                    !userTimestamps[index] ||
-                    !userTimestamps[index][roomId] ||
-                    parseInt(userTimestamps[index][roomId], 10) > lastMsgTimestamp
-                );
+export function Messaging() {
+    return {
+        getUnreadCount: async (uid: string | number): Promise<number> => {
+            if (!(parseInt(uid as string, 10) > 0)) {
+                return 0;
             }
             
+            const result = await db.sortedSetCard(`uid:${uid}:chat:rooms:unread`);
+            return result;
+        },
+
+        pushUnreadCount: async (uids: string[] | string, data: any = null): Promise<undefined> => {
+            if (!Array.isArray(uids)) {
+                uids = [uids];
+            }
+            
+            uids = uids.filter(uid => parseInt(uid as string, 10) > 0);
+            
+            if (!uids.length) {
+                return;
+            }
+            
+            uids.forEach((uid) => {
+                io.in(`uid_${uid}`).emit('event:unread.updateChatCount', data);
+            });
+        },
+
+        markRead: async (uid: string | number, roomId: string): Promise<undefined> => {
+            await Promise.all([
+                db.sortedSetRemove(`uid:${uid}:chat:rooms:unread`, roomId),
+                db.setObjectField(`uid:${uid}:chat:rooms:read`, roomId, Date.now()),
+            ]);
+        },
+
+        hasRead: async (uids: string[], roomId: string): Promise<boolean[]> => {
+            if (!uids.length) {
+                return [];
+            }
+            
+            const roomData = await this.getRoomData(roomId);
+            if (!roomData) {
+                return uids.map(() => false);
+            }
+            
+            if (roomData.public) {
+                const [userTimestamps, mids] = await Promise.all([
+                    db.getObjectsFields(uids.map(uid => `uid:${uid}:chat:rooms:read`), [roomId]),
+                    db.getSortedSetRevRangeWithScores(`chat:room:${roomId}:mids`, 0, 0),
+                ]);
+                
+                const lastMsgTimestamp = mids[0] ? mids[0].score : 0;
+                return uids.map(
+                    (uid, index) =>
+                        !userTimestamps[index] ||
+                        !userTimestamps[index][roomId] ||
+                        parseInt(userTimestamps[index][roomId], 10) > lastMsgTimestamp
+                );
+            }
+                
             const isMembers = await db.isMemberOfSortedSets(
                 uids.map(uid => `uid:${uid}:chat:rooms:unread`),
                 roomId
             );
-            
+                
             return uids.map((uid, index) => !isMembers[index]);
-        };
-        
-        Messaging.markAllRead = async (uid: string | number): Promise<void> => {
+        },
+
+        markAllRead: async (uid: string | number): Promise<undefined> => {
             await db.delete(`uid:${uid}:chat:rooms:unread`);
-        };
-        
-        Messaging.markUnread = async (uids: string[], roomId: string): Promise<void> => {
-            const exists = await Messaging.roomExists(roomId);
+        },
+
+        markUnread: async (uids: string[], roomId: string): Promise<undefined> => {
+            const exists = await this.roomExists(roomId);
             if (!exists) {
-                return;
+              return;
             }
-            
+      
             const keys = uids.map(uid => `uid:${uid}:chat:rooms:unread`);
             await db.sortedSetsAdd(keys, Date.now(), roomId);
-        };
+          },
+      
+          roomExists: async (roomId: string): Promise<boolean> => {
+            const exists = await db.exists(`chat:room:${roomId}`);
+            return exists > 0;
+          },
+      
+          getRoomData: async (roomId: string): Promise<any> => {
+            return await db.getObject(`chat:room:${roomId}`);
+          }
     };
+};
