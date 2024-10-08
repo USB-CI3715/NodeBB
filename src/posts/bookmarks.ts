3403726695750d6bfc99bd0d7d1349ef8382a17b
neonhh <nj.herrera14@gmail.com>
2024-10-08 10:31:31 -0400

    Annotates dependencies from untraduced files

--- src/posts/bookmarks.ts
+++ src/posts/bookmarks.ts
@@ -19,32 +19,34 @@ interface BookmarkStatus {
 
 export default class Posts {
 //module.exports = function (Posts) {
-	public static async bookmark(pid: number, uid: number): Promise<any> {
+	public static async bookmark(pid: any, uid: any): Promise<any> {
 	//Posts.bookmark = async function (pid, uid) {
 		return await this.toggleBookmark('bookmark', pid, uid);
 	};
 
-	public static async unbookmark(pid: number, uid: number): Promise<any> {
+	public static async unbookmark(pid: any, uid: any): Promise<any> {
 	//Posts.unbookmark = async function (pid, uid) {
 		return await this.toggleBookmark('unbookmark', pid, uid);
 	};
 
-	private static async toggleBookmark(type: string, pid: number, uid: number): Promise<BookmarkStatus> {
+	private static async toggleBookmark(type: string, pid: any, uid: any): Promise<BookmarkStatus  | null> {
 	//async function toggleBookmark(type, pid, uid) {
-		if (parseInt(uid, 10) <= 0) {
+		if (uid as number <= 0) {
 			throw new Error('[[error:not-logged-in]]');
 		}
 
 		const isBookmarking = type === 'bookmark';
 
+		// The next line calls a function in a module that has not been updated to TS (getPostField - src/posts/data.js). 
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 		const postData: PostData = await Posts.getPostFields(pid, ['pid', 'uid']);
-		const hasBookmarked: boolean = await Posts.hasBookmarked(pid, uid);
+		const hasBookmarked: boolean = await Posts.hasBookmarked(pid, uid);		
 		/*
-		const [postData, hasBookmarked] = await Promise.all([
+		const [postData: PostData, hasBookmarked: boolean] = await Promise.all([
 			Posts.getPostFields(pid, ['pid', 'uid']),
 			Posts.hasBookmarked(pid, uid),
 		]);
-*/
+		*/
 
 		if (isBookmarking && hasBookmarked) {
 			throw new Error('[[error:already-bookmarked]]');
@@ -61,6 +63,9 @@ export default class Posts {
 		}
 		await db[isBookmarking ? 'setAdd' : 'setRemove'](`pid:${pid}:users_bookmarked`, uid);
 		postData.bookmarks = await db.setCount(`pid:${pid}:users_bookmarked`);
+		
+		// The next line calls a function in a module that has not been updated to TS (setPostField - src/posts/data.js). 
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 		await Posts.setPostField(pid, 'bookmarks', postData.bookmarks);
 
 		plugins.hooks.fire(`action:post.${type}`, {
@@ -78,7 +83,7 @@ export default class Posts {
 
 	public static async hasBookmarked(pid: number, uid: number): Promise<any> {
 	//Posts.hasBookmarked = async function (pid, uid) {
-		if (parseInt(uid, 10) <= 0) {
+		if (uid as number <= 0) {
 			return Array.isArray(pid) ? pid.map(() => false) : false;
 		}
 
