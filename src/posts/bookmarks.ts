59f18af0b69ad019df1998659899469438547e95
neonhh <nj.herrera14@gmail.com>
2024-10-08 11:01:34 -0400

    Refines comments

--- src/posts/bookmarks.ts
+++ src/posts/bookmarks.ts
@@ -1,9 +1,7 @@
 'use strict';
-//Switching 'require' to 'import'
+//Switching 'require' to 'import' for compatibility
 import db from '../database';
 import plugins from '../plugins';
-//const db = require('../database');
-//const plugins = require('../plugins');
 
 //Creating interfaces for complex types
 interface PostData{
@@ -18,35 +16,29 @@ interface BookmarkStatus {
 }
 
 export default class Posts {
-//module.exports = function (Posts) {
-	public static async bookmark(pid: any, uid: any): Promise<any> {
-	//Posts.bookmark = async function (pid, uid) {
+	//bookmark and unbookmark are public methods. Not sure what the types of pid and uid should be.
+	// the return value is the same type as the return value of toggleBookmark
+	public static async bookmark(pid: any, uid: any): Promise<BookmarkStatus  | null> {
 		return await this.toggleBookmark('bookmark', pid, uid);
 	};
 
-	public static async unbookmark(pid: any, uid: any): Promise<any> {
-	//Posts.unbookmark = async function (pid, uid) {
+	public static async unbookmark(pid: any, uid: any): Promise<BookmarkStatus  | null> {
 		return await this.toggleBookmark('unbookmark', pid, uid);
 	};
 
+	//toggleBookmark is a private method. In this case whenever the method is called, the type is a string.
 	private static async toggleBookmark(type: string, pid: any, uid: any): Promise<BookmarkStatus  | null> {
-	//async function toggleBookmark(type, pid, uid) {
-		if (uid as number <= 0) {
+		if (uid as number <= 0) { // uid needs to be casted to a number
 			throw new Error('[[error:not-logged-in]]');
 		}
 
 		const isBookmarking = type === 'bookmark';
 
-		// The next line calls a function in a module that has not been updated to TS (getPostField - src/posts/data.js). 
+		// The next line calls a function in a module that has not been updated to TS (getPostFields - src/posts/data.js). 
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 		const postData: PostData = await Posts.getPostFields(pid, ['pid', 'uid']);
-		const hasBookmarked: boolean = await Posts.hasBookmarked(pid, uid);		
-		/*
-		const [postData: PostData, hasBookmarked: boolean] = await Promise.all([
-			Posts.getPostFields(pid, ['pid', 'uid']),
-			Posts.hasBookmarked(pid, uid),
-		]);
-		*/
+		const hasBookmarked: boolean = await Posts.hasBookmarked(pid, uid);
+		// I avoid using promise.all here because it wouldn't allow me to add types to postData and hasBookmarked		
 
 		if (isBookmarking && hasBookmarked) {
 			throw new Error('[[error:already-bookmarked]]');
@@ -81,8 +73,7 @@ export default class Posts {
 		};
 	}
 
-	public static async hasBookmarked(pid: number, uid: number): Promise<any> {
-	//Posts.hasBookmarked = async function (pid, uid) {
+	public static async hasBookmarked(pid: any, uid: any): Promise<any> {
 		if (uid as number <= 0) {
 			return Array.isArray(pid) ? pid.map(() => false) : false;
 		}
