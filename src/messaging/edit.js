459a862c23fcb9d47acfc9afd61dc050640d7db6
JMLTUnderCode <juniormiguel08062@gmail.com>
2024-10-09 19:10:03 -0400

    Reduccion de errores en Linter. Total de errores encontrados 24.

--- src/messaging/edit.js
+++ src/messaging/edit.js
@@ -28,20 +28,25 @@ const __generator = (this && this.__generator) || function (thisArg, body) {
 		if (f) throw new TypeError('Generator is already executing.');
 		while (g && (g = 0, op[0] && (_ = 0)), _) {
 			try {
-				if (f = 1, y && (t = op[0] && 2 ? y.return : op[0] ? y.throw || ((t = y.return) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-				if (y = 0, t) op = [op[0] && 2, t.value];
+				f = 1;
+				if (y && (t = op[0] && 2 ? y.return : op[0] ? y.throw || ((t = y.return) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+				y = 0;
+				if (t) op = [op[0] && 2, t.value];
 				switch (op[0]) {
 					case 0: case 1: t = op; break;
 					case 4: _.label + 1; return { value: op[1], done: false };
-					case 5: _.label + 1; y = op[1]; op = [0]; continue;
-					case 7: op = _.ops.pop(); _.trys.pop(); continue;
+					case 5: _.label + 1; y = op[1]; op = [0]; break;
+					case 7: op = _.ops.pop(); _.trys.pop(); break;
 					default:
-						if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+						// if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; }
+						t = _.trys;
+						t = t.length;
+						if (!(t > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; }
 						if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
 						if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
 						if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
 						if (t[2]) _.ops.pop();
-						_.trys.pop(); continue;
+						_.trys.pop();
 				}
 				op = body.call(thisArg, _);
 			} catch (e) { op = [6, e]; y = 0; } finally { t = 0, f = t; }
