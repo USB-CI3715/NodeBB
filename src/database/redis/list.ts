986b47b7adab3c5bffce2cf139438aacb53970d1
AnyaMarcanito <19-10336@usb.ve>
2024-10-10 23:05:19 -0400

    Agregadas lineas para deshabilitar eslint por tipaje de tipo any en list.ts + Agregado typeRoots en el tsconfig.json

--- src/database/redis/list.ts
+++ src/database/redis/list.ts
@@ -1,6 +1,9 @@
-'use strict';
-import { createClient, RedisClientType } from 'redis';
-import * as helpers from './helpers';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+import { RedisClientType } from 'redis';
+import { execBatch } from './helpers';
 
 interface Module {
     client: RedisClientType;
@@ -14,65 +17,63 @@ interface Module {
 }
 
 module.exports = function (module: Module) {
+	module.listPrepend = async function (key: string, value: any): Promise<void> {
+		if (!key) {
+			return;
+		}
+		await module.client.lPush(key, value);
+	};
 
 
-    module.listPrepend = async function (key: string, value: any): Promise<void> {
-        if (!key) {
-            return;
-        }
-        await module.client.lPush(key, value);
-    };
+	module.listAppend = async function (key: string, value: any): Promise<void> {
+		if (!key) {
+			return;
+		}
+		await module.client.rPush(key, value);
+	};
 
 
-    module.listAppend = async function (key: string, value: any): Promise<void> {
-        if (!key) {
-            return;
-        }
-        await module.client.rPush(key, value);
-    };
+	module.listRemoveLast = async function (key: string): Promise<string | null> {
+		if (!key) {
+			return null;
+		}
+		const result = await module.client.rPop(key);
+		return result || null;
+	};
 
 
-    module.listRemoveLast = async function (key: string): Promise<string | null> {
-        if (!key) {
-            return null;
-        }
-        const result = await module.client.rPop(key);
-        return result || null;
-    };
+	module.listRemoveAll = async function (key: string, value: string[] | string): Promise<void> {
+		if (!key) {
+			return;
+		}
+		if (Array.isArray(value)) {
+			const batch = module.client.multi();
+			value.forEach(value => batch.lRem(key, 0, value));
+			await execBatch(batch);
+		} else {
+			await module.client.lRem(key, 0, value);
+		}
+	};
 
 
-    module.listRemoveAll = async function (key: string, value: string[] | string): Promise<void> {
-        if (!key) {
-            return;
-        }
-        if (Array.isArray(value)) {
-            const batch = module.client.multi();
-            value.forEach(value => batch.lRem(key, 0, value));
-            await helpers.execBatch(batch);
-        } else {
-            await module.client.lRem(key, 0, value);
-        }
-    };
+	module.listTrim = async function (key: string, start: number, stop: number): Promise<void> {
+		if (!key) {
+			return;
+		}
+		await module.client.lTrim(key, start, stop);
+	};
 
 
-    module.listTrim = async function (key: string, start: number, stop: number): Promise<void> {
-        if (!key) {
-            return;
-        }
-        await module.client.lTrim(key, start, stop);
-    };
+	module.getListRange = async function (key: string, start: number, stop: number): Promise<string[] | null> {
+		if (!key) {
+			return null;
+		}
+		const result = await module.client.lRange(key, start, stop);
+		return result || null;
+	};
 
 
-    module.getListRange = async function (key: string, start: number, stop: number): Promise<string[] | null> {
-        if (!key) {
-            return null;
-        }
-        const result = await module.client.lRange(key, start, stop);
-        return result || null;
-    };
-
-
-    module.listLength = async function (key: string): Promise<number> {
-        return await module.client.lLen(key);
-    };
+	module.listLength = async function (key: string): Promise<number> {
+		return await module.client.lLen(key);
+	};
 };
