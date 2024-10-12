a93ba73d0fb27a66d09a7bd672bfe87e8af73f42
Jose Alfonzo <jralfonzop@gmail.com>
2024-10-12 15:04:43 -0400

    Agregar interfaces al .ts. Posee 21 errores de eslint que no se arreglaron con npm run lint -- --fix

--- src/controllers/write/categories.ts
+++ src/controllers/write/categories.ts
@@ -1,107 +1,122 @@
-'use strict';
-
 import categories from '../../categories';
 import meta from '../../meta';
 import api from '../../api';
-
 import helpers from '../helpers';
 
-export const Categories: any = {};
+interface Params {
+    cid: string;
+    privilege?: string;
+    uid?: string;
+}
 
-Categories.list = async (req: any, res: any) => {
-    helpers.formatApiResponse(200, res, await api.categories.list(req));
+interface Request {
+    params: Params;
+    body: any;
+    query: { start?: number };
+    method: string;
+}
+
+interface Response {
+    status: (code: number) => void;
+    json: (data: any) => void;
+}
+
+const Categories = {
+	list: async (req: Request, res: Response) => {
+		await helpers.formatApiResponse(200, res, await api.categories.list(req));
+	},
+
+	get: async (req: Request, res: Response) => {
+		await helpers.formatApiResponse(200, res, await api.categories.get(req, req.params));
+	},
+
+	create: async (req: Request, res: Response) => {
+		const response = await api.categories.create(req, req.body);
+		await helpers.formatApiResponse(200, res, response);
+	},
+
+	update: async (req: Request, res: Response) => {
+		await api.categories.update(req, {
+			cid: req.params.cid,
+			values: req.body,
+		});
+
+		const categoryObjs = await categories.getCategories([req.params.cid]);
+		await helpers.formatApiResponse(200, res, categoryObjs[0]);
+	},
+
+	delete: async (req: Request, res: Response) => {
+		await api.categories.delete(req, { cid: req.params.cid });
+		await helpers.formatApiResponse(200, res);
+	},
+
+	getTopicCount: async (req: Request, res: Response) => {
+		await helpers.formatApiResponse(200, res, await api.categories.getTopicCount(req, { ...req.params }));
+	},
+
+	getPosts: async (req: Request, res: Response) => {
+		const posts = await api.categories.getPosts(req, { ...req.params });
+		await helpers.formatApiResponse(200, res, { posts });
+	},
+
+	getChildren: async (req: Request, res: Response) => {
+		const { cid } = req.params;
+		const { start } = req.query;
+		await helpers.formatApiResponse(200, res, await api.categories.getChildren(req, { cid, start }));
+	},
+
+	getTopics: async (req: Request, res: Response) => {
+		const { cid } = req.params;
+		const result = await api.categories.getTopics(req, { ...req.query, cid });
+
+		await helpers.formatApiResponse(200, res, result);
+	},
+
+	setWatchState: async (req: Request, res: Response) => {
+		const { cid } = req.params;
+		let { uid, state } = req.body;
+
+		if (req.method === 'DELETE') {
+			state = categories.watchStates[meta.config.categoryWatchState];
+		} else if (Object.keys(categories.watchStates).includes(state)) {
+			state = categories.watchStates[state];
+		} else {
+			throw new Error('[[error:invalid-data]]');
+		}
+
+		const { cids: modified } = await api.categories.setWatchState(req, { cid, state, uid });
+		await helpers.formatApiResponse(200, res, { modified });
+	},
+
+	getPrivileges: async (req: Request, res: Response) => {
+		const privilegeSet = await api.categories.getPrivileges(req, { cid: req.params.cid });
+		await helpers.formatApiResponse(200, res, privilegeSet);
+	},
+
+	setPrivilege: async (req: Request, res: Response) => {
+		const { cid, privilege } = req.params;
+
+		await api.categories.setPrivilege(req, {
+			cid,
+			privilege,
+			member: req.body.member,
+			set: req.method === 'PUT',
+		});
+
+		const privilegeSet = await api.categories.getPrivileges(req, { cid: req.params.cid });
+		await helpers.formatApiResponse(200, res, privilegeSet);
+	},
+
+	setModerator: async (req: Request, res: Response) => {
+		await api.categories.setModerator(req, {
+			cid: req.params.cid,
+			member: req.params.uid,
+			set: req.method === 'PUT',
+		});
+
+		const privilegeSet = await api.categories.getPrivileges(req, { cid: req.params.cid });
+		await helpers.formatApiResponse(200, res, privilegeSet);
+	},
 };
 
-Categories.get = async (req: any, res: any) => {
-    helpers.formatApiResponse(200, res, await api.categories.get(req, req.params));
-};
-
-Categories.create = async (req: any, res: any) => {
-    const response = await api.categories.create(req, req.body);
-    helpers.formatApiResponse(200, res, response);
-};
-
-Categories.update = async (req: any, res: any) => {
-    await api.categories.update(req, {
-        cid: req.params.cid,
-        values: req.body,
-    });
-
-    const categoryObjs = await categories.getCategories([req.params.cid]);
-    helpers.formatApiResponse(200, res, categoryObjs[0]);
-};
-
-Categories.delete = async (req: any, res: any) => {
-    await api.categories.delete(req, { cid: req.params.cid });
-    helpers.formatApiResponse(200, res);
-};
-
-Categories.getTopicCount = async (req: any, res: any) => {
-    helpers.formatApiResponse(200, res, await api.categories.getTopicCount(req, { ...req.params }));
-};
-
-Categories.getPosts = async (req: any, res: any) => {
-    const posts = await api.categories.getPosts(req, { ...req.params });
-    helpers.formatApiResponse(200, res, { posts });
-};
-
-Categories.getChildren = async (req: any, res: any) => {
-    const { cid } = req.params;
-    const { start } = req.query;
-    helpers.formatApiResponse(200, res, await api.categories.getChildren(req, { cid, start }));
-};
-
-Categories.getTopics = async (req: any, res: any) => {
-    const { cid } = req.params;
-    const result = await api.categories.getTopics(req, { ...req.query, cid });
-
-    helpers.formatApiResponse(200, res, result);
-};
-
-Categories.setWatchState = async (req: any, res: any) => {
-    const { cid } = req.params;
-    let { uid, state } = req.body;
-
-    if (req.method === 'DELETE') {
-        // DELETE is always setting state to system default in acp
-        state = categories.watchStates[meta.config.categoryWatchState];
-    } else if (Object.keys(categories.watchStates).includes(state)) {
-        state = categories.watchStates[state]; // convert to integer for backend processing
-    } else {
-        throw new Error('[[error:invalid-data]]');
-    }
-
-    const { cids: modified } = await api.categories.setWatchState(req, { cid, state, uid });
-
-    helpers.formatApiResponse(200, res, { modified });
-};
-
-Categories.getPrivileges = async (req: any, res: any) => {
-    const privilegeSet = await api.categories.getPrivileges(req, { cid: req.params.cid });
-    helpers.formatApiResponse(200, res, privilegeSet);
-};
-
-Categories.setPrivilege = async (req: any, res: any) => {
-    const { cid, privilege } = req.params;
-
-    await api.categories.setPrivilege(req, {
-        cid,
-        privilege,
-        member: req.body.member,
-        set: req.method === 'PUT',
-    });
-
-    const privilegeSet = await api.categories.getPrivileges(req, { cid: req.params.cid });
-    helpers.formatApiResponse(200, res, privilegeSet);
-};
-
-Categories.setModerator = async (req: any, res: any) => {
-    await api.categories.setModerator(req, {
-        cid: req.params.cid,
-        member: req.params.uid,
-        set: req.method === 'PUT',
-    });
-
-    const privilegeSet = await api.categories.getPrivileges(req, { cid: req.params.cid });
-    helpers.formatApiResponse(200, res, privilegeSet);
-};
+export default Categories;
