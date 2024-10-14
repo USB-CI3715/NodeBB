4892bf294056fdc0ae14875931354b77cae645c6
César González <cedgo1997@gmail.com>
2024-10-14 05:16:54 -0400

    FIX linter errors

--- src/utils.js
+++ src/utils.js
@@ -1,32 +1,32 @@
-'use strict';
+"use strict";
 
-const crypto = require('crypto');
-const nconf = require('nconf');
-const path = require('node:path');
+const crypto = require("crypto");
+const nconf = require("nconf");
+const path = require("node:path");
+
+const elapsedTimeSince = function (start) {
+	const diff = process.hrtime(start);
+	return diff[0] * 1e3 + diff[1] / 1e6;
+};
 
 process.profile = function (operation, start) {
 	console.log(
-		'%s took %d milliseconds',
+		"%s took %d milliseconds",
 		operation,
 		process.elapsedTimeSince(start)
 	);
 };
 
-var elapsedTimeSince = function (start) {
-	const diff = process.hrtime(start);
-	return diff[0] * 1e3 + diff[1] / 1e6;
-};
-
 process.elapsedTimeSince = elapsedTimeSince;
-const utils = { ...require('../public/src/utils.common') };
+const utils = { ...require("../public/src/utils.common") };
 
 utils.elapsedTimeSince = elapsedTimeSince;
 
 utils.getLanguage = function () {
-	const meta = require('./meta');
+	const meta = require("./meta");
 	return meta.config && meta.config.defaultLang
 		? meta.config.defaultLang
-		: 'en-GB';
+		: "en-GB";
 };
 
 utils.generateUUID = function () {
@@ -36,24 +36,24 @@ utils.generateUUID = function () {
 	rnd[6] = (rnd[6] & 0x0f) | 0x40;
 	rnd[8] = (rnd[8] & 0x3f) | 0x80;
 	/* eslint-enable no-bitwise */
-	rnd = rnd.toString('hex').match(/(.{8})(.{4})(.{4})(.{4})(.{12})/);
+	rnd = rnd.toString("hex").match(/(.{8})(.{4})(.{4})(.{4})(.{12})/);
 	rnd.shift();
-	return rnd.join('-');
+	return rnd.join("-");
 };
 
 utils.getSass = function () {
 	try {
-		const sass = require('sass-embedded');
+		const sass = require("sass-embedded");
 		return sass;
 	} catch (_err) {
-		return require('sass');
+		return require("sass");
 	}
 };
 
 utils.getFontawesomePath = function () {
-	let packageName = '@fortawesome/fontawesome-free';
-	if (nconf.get('fontawesome:pro') === true) {
-		packageName = '@fortawesome/fontawesome-pro';
+	let packageName = "@fortawesome/fontawesome-free";
+	if (nconf.get("fontawesome:pro") === true) {
+		packageName = "@fortawesome/fontawesome-pro";
 	}
 	const pathToMainFile = require.resolve(packageName);
 	// main file will be in `js/fontawesome.js` - we need to go up two directories to get to the root of the package
@@ -62,12 +62,12 @@ utils.getFontawesomePath = function () {
 };
 
 utils.getFontawesomeStyles = function () {
-	let styles = nconf.get('fontawesome:styles') || '*';
+	let styles = nconf.get("fontawesome:styles") || "*";
 	// "*" is a special case, it means all styles, spread is used to support both string and array (["*"])
-	if ([...styles][0] === '*') {
-		styles = ['solid', 'brands', 'regular'];
-		if (nconf.get('fontawesome:pro')) {
-			styles.push('light', 'thin', 'sharp', 'duotone');
+	if ([...styles][0] === "*") {
+		styles = ["solid", "brands", "regular"];
+		if (nconf.get("fontawesome:pro")) {
+			styles.push("light", "thin", "sharp", "duotone");
 		}
 	}
 	if (!Array.isArray(styles)) {
@@ -78,7 +78,7 @@ utils.getFontawesomeStyles = function () {
 
 utils.getFontawesomeVersion = function () {
 	const fontawesomePath = utils.getFontawesomePath();
-	const packageJson = require(path.join(fontawesomePath, 'package.json'));
+	const packageJson = require(path.join(fontawesomePath, "package.json"));
 	return packageJson.version;
 };
 
