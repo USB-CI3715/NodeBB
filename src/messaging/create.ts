0707d24c9db6f7a3f74f6aa38768b59196ef0813
Yerimar <yeritachan@gmail.com>
2024-10-13 23:45:54 -0400

    Declaracion de tipos y creacion de interfaz de Messaging

---
+++ src/messaging/create.ts
@@ -0,0 +1,152 @@
+// 'use strict';
+
+import * as _ from 'lodash';
+
+import * as plugins from '../meta';
+import * as meta from '../plugins';
+import * as db from '../database';
+import * as user from '../user';
+import * as utils from '../utils';
+
+interface Messaging {
+    sendMessage(data: any): Promise<void>;
+    checkContent(content: string);
+    addMessage(data: any);
+    addSystemMessage(content: string, uid:string, roomId: number);
+    addRoomToUsers(roomId: number, uids: string[], timestamp: Date);
+    addMessageToRoom(roomId: number, mid: string, timestamp: Date);
+	isUserInRoom(uid: string, roomId: number): Promise<boolean | boolean[]>;
+	getRoomData(roomId: number): Promise<{ public: boolean }>;
+	canViewMessage(toMid:number, roomId:number, uid:string): Promise<boolean | boolean[]> ;
+	isNewSet(uid: string, roomId: number, timestamp: Date): Promise<boolean>;
+	markRead(uid: string, roomId: number): Promise<void>;
+	getUidsInRoom(roomId: number, start: number, stop: number): string[];
+	markUnread(uids: string[], roomId:number): Promise<void>;
+	getMessagesData(mids:number[], uid:string, roomId:number, isNew:boolean): Promise<unknown>;
+	notifyUsersInRoom(uid:string, roomId:number, message:unknown): Promise<void>;
+
+}
+
+module.exports = function (Messaging: Messaging) {
+	Messaging.sendMessage = async (data: any): Promise<void> => {
+		await Messaging.checkContent(data.content);
+		
+		const inRoom = await Messaging.isUserInRoom(data.uid, data.roomId);
+		if (!inRoom) {
+			throw new Error('[[error:not-allowed]]');
+		}
+
+		return await Messaging.addMessage(data);
+	};
+
+	Messaging.checkContent = async (content: string) => {
+		if (!content) {
+			throw new Error('[[error:invalid-chat-message]]');
+		}
+
+		const maximumChatMessageLength = meta.config.maximumChatMessageLength || 1000;
+		content = String(content).trim();
+		let { length } = content;
+		({ content, length } = await plugins.hooks.fire('filter:messaging.checkContent', { content, length }));
+		if (!content) {
+			throw new Error('[[error:invalid-chat-message]]');
+		}
+		if (length > maximumChatMessageLength) {
+			throw new Error(`[[error:chat-message-too-long, ${maximumChatMessageLength}]]`);
+		}
+	};
+
+	Messaging.addMessage = async (data:any) => {
+		const { uid, roomId } = data;
+		const roomData = await Messaging.getRoomData(roomId);
+		if (!roomData) {
+			throw new Error('[[error:no-room]]');
+		}
+		if (data.toMid) {
+			if (!utils.isNumber(data.toMid)) {
+				throw new Error('[[error:invalid-mid]]');
+			}
+			if (!await Messaging.canViewMessage(data.toMid, roomId, uid)) {
+				throw new Error('[[error:no-privileges]]');
+			}
+		}
+		const mid = await db.incrObjectField('global', 'nextMid');
+		const timestamp = data.timestamp || Date.now();
+		let message: any = {
+			mid: mid,
+			content: String(data.content),
+			timestamp: timestamp,
+			fromuid: uid,
+			roomId: roomId,
+		};
+		if (data.toMid) {
+			message.toMid = data.toMid;
+		}
+		if (data.system) {
+			message.system = data.system;
+		}
+
+		if (data.ip) {
+			message.ip = data.ip;
+		}
+
+		message = await plugins.hooks.fire('filter:messaging.save', message);
+		await db.setObject(`message:${mid}`, message);
+		const isNewSet = await Messaging.isNewSet(uid, roomId, timestamp);
+
+		const tasks = [
+			Messaging.addMessageToRoom(roomId, mid, timestamp),
+			Messaging.markRead(uid, roomId),
+			db.sortedSetAdd('messages:mid', timestamp, mid),
+			db.incrObjectField('global', 'messageCount'),
+		];
+		if (data.toMid) {
+			tasks.push(db.sortedSetAdd(`mid:${data.toMid}:replies`, timestamp, mid));
+		}
+		if (roomData.public) {
+			tasks.push(
+				db.sortedSetAdd('chat:rooms:public:lastpost', timestamp, roomId)
+			);
+		} else {
+			let uids = await Messaging.getUidsInRoom(roomId, 0, -1);
+			uids = await user.blocks.filterUids(uid, uids);
+			tasks.push(
+				Messaging.addRoomToUsers(roomId, uids, timestamp),
+				Messaging.markUnread(uids.filter((uid: string) => uid !== data.uid), roomId), // Revisar en el futuro
+			);
+		}
+		await Promise.all(tasks);
+
+		const messages = await Messaging.getMessagesData([mid], uid, roomId, true);
+		if (!messages || !messages[0]) {
+			return null;
+		}
+
+		messages[0].newSet = isNewSet;
+		plugins.hooks.fire('action:messaging.save', { message: message, data: data });
+		return messages[0];
+	};
+
+	Messaging.addSystemMessage = async (content: string, uid:string, roomId: number) => {
+		const message = await Messaging.addMessage({
+			content: content,
+			uid: uid,
+			roomId: roomId,
+			system: 1,
+		});
+		Messaging.notifyUsersInRoom(uid, roomId, message);
+	};
+
+	Messaging.addRoomToUsers = async (roomId: number, uids: string[], timestamp: Date) => {
+		if (!uids.length) {
+			return;
+		}
+		const keys = _.uniq(uids).map(uid => `uid:${uid}:chat:rooms`);
+		await db.sortedSetsAdd(keys, timestamp, roomId);
+	};
+
+	Messaging.addMessageToRoom = async (roomId: number, mid: string, timestamp: Date) => {
+		await db.sortedSetAdd(`chat:room:${roomId}:mids`, timestamp, mid);
+		await db.incrObjectField(`chat:room:${roomId}`, 'messageCount');
+	};
+};
