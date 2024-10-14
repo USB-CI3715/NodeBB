a7b7dafd8bf1f1d4cc7372b0979d03278b2ea2c5
Jeam <19-10234@usb.ve>
2024-10-13 22:57:58 -0400

    Fix linter errors in create function

--- src/posts/create.ts
+++ src/posts/create.ts
@@ -33,3 +33,35 @@ interface Post {
     isMain?: boolean;
     deleted?: boolean;
 }
+
+function createPosts(Posts: Post) {
+	async function checkToPid(toPid: number, uid: number): Promise<void> {
+		const [toPost, canViewToPid] = await Promise.all([
+			// La siguiente línea llama a una función en un módulo que aún no ha sido actualizado a TS
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+			Posts.getPostFields(toPid, ['pid', 'deleted']),
+			privileges.posts.can('posts:view_deleted', toPid, uid),
+		]) as [Post, boolean];
+		// La siguiente línea llama a una función en un módulo que aún no ha sido actualizado a TS
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+		const toPidExists = !!toPost.pid;
+		// La siguiente línea llama a una función en un módulo que aún no ha sido actualizado a TS
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+		if (!toPidExists || (toPost.deleted && !canViewToPid)) {
+			throw new Error('[[error:invalid-pid]]');
+		}
+	}
+	async function addReplyTo(postData: Post, timestamp: number): Promise<void> {
+		if (!postData.toPid) {
+			return;
+		}
+		await Promise.all([
+			// La siguiente línea llama a una función en un módulo que aún no ha sido actualizado a TS
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+			db.sortedSetAdd(`pid:${postData.toPid}:replies`, timestamp, postData.pid),
+			// La siguiente línea llama a una función en un módulo que aún no ha sido actualizado a TS
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+			db.incrObjectField(`post:${postData.toPid}`, 'replies'),
+		]);
+	}
+}
